b825f395608034605574d7a9a870c09c22e90073
Smarty Bit <karpukhin@gmail.com>
2016-03-01 17:55:33 +0300

    removed default nginx config at launch

--- init.sh
+++ init.sh
@@ -4,7 +4,7 @@ sudo mkdir -p /home/box/web/public/{img,css,js}
 sudo mkdir /home/box/web/{uploads,etc}
 
 sudo touch  /home/box/web/public/js/test.js
-
+sudo rm  /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 
 sudo nginx
