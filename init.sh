014ee87a5b4ef418e3868c317a4f527e937ef195
smartybit <karpukhin@gmail.com>
2016-03-05 18:31:49 +0300

    directory confusion fix

--- init.sh
+++ init.sh
@@ -5,10 +5,10 @@ sudo mkdir /home/box/web/{uploads,etc}
 
 sudo touch  /home/box/web/public/js/test.js
 sudo rm  /etc/nginx/sites-enabled/default
-sudo ln -s /home/box/web/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
-sudo ln -s /home/box/web/web/etc/hello.py  /etc/gunicorn.d/hello.py
+sudo ln -s /home/box/stepic_webtech1/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo ln -s /home/box/stepic_webtech1/web/etc/hello.py  /etc/gunicorn.d/hello.py
 
-cd /home/box/web/web
+cd /home/box/stepic_webtech1/web
 gunicorn -c /etc/gunicorn.d/hello.py hello:app&
 
 sudo nginx
