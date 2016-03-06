367c5a36d7954a2603f4a82dfb5a5cf19ab73484
smartybit <karpukhin@gmail.com>
2016-03-06 06:23:14 +0000

    added symlink to hello.py for succsessful chalange validation

--- init.sh
+++ init.sh
@@ -7,6 +7,7 @@ sudo touch  /home/box/web/public/js/test.js
 sudo rm  /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/stepic_webtech1/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo ln -s /home/box/stepic_webtech1/web/etc/hello.py  /etc/gunicorn.d/hello.py
+sudo ln -s /home/box/stepic_webtech1/web/hello.py  /home/box/web/hello.py
 
 cd /home/box/stepic_webtech1/web
 gunicorn -c /etc/gunicorn.d/hello.py hello:app&
