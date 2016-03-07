e6f1f825baddbb34710050a0296e50e92901addc
Your Name <you@example.com>
2016-03-07 16:13:02 +0000

    guniconf config changed to satisfy autostart script

--- web/etc/ask.py
+++ web/etc/ask.py
@@ -1,2 +1,13 @@
-bind = "0.0.0.0:8000"
-chdir = '/home/box/web/ask/ask'
+CONFIG = {
+    # 'mode': 'wsgi',
+    'working_dir': '/home/box/web/ask',
+    # 'python': '/usr/bin/python',
+    'args': (
+         '--bind=127.0.0.1:8000',
+        '--workers=2',
+        '--timeout=60',
+        'ask.wsgi',
+    ),
+}
+#bind = "0.0.0.0:8000"
+#chdir = '/home/box/web/ask/ask'
