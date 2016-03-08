7a4af2ab7bdce96fd8c36ecd9ac9ff8dd368d5df
Your Name <you@example.com>
2016-03-08 05:53:19 +0000

    Guniconf congiguration fix

--- web/etc/ask.py
+++ web/etc/ask.py
@@ -3,7 +3,7 @@ CONFIG = {
     'working_dir': '/home/box/web/ask',
     # 'python': '/usr/bin/python',
     'args': (
-         '--bind=127.0.0.1:8000',
+         '--bind=0.0.0.0:8000',
         '--workers=2',
         '--timeout=60',
         'ask.wsgi',
