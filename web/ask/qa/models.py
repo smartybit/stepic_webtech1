d206617bf0608d0ca1df916ffc9e23fe1cfe8807
smartybit <karpukhin@gmail.com>
2016-03-17 12:55:51 +0300

    Model changes to suit test on stepic.org #4

--- web/ask/qa/models.py
+++ web/ask/qa/models.py
@@ -19,7 +19,7 @@ class Question(models.Model):
 
 class Answer(models.Model):
     text = models.TextField()
-    added_at = models.DateTimeField(blank=True)
+    added_at = models.DateTimeField(blank=True, auto_now_add = True)
     question = models.ForeignKey(Question, on_delete = models.CASCADE)
     author = models.ForeignKey(User, on_delete = models.CASCADE)
     def __unicode__(self):
