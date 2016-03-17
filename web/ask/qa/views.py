2d3330d229052523d2fb69da682235f8e754579a
smartybit <karpukhin@gmail.com>
2016-03-17 09:15:49 +0000

    Trivial question page with answers added

--- web/ask/qa/views.py
+++ web/ask/qa/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from django.http import HttpResponse 
 import qa.dataload as dataload
@@ -11,12 +11,34 @@ def test(request, *args, **kwargs):
 
 
 def mainpage(request, *args, **kwargs):
-    question_set = models.Question.objects.order_by('raiting')
+    question_set = models.Question.objects.order_by('-added_at', '-raiting')
+
     paginator, page = paginate(request, question_set)
     paginator.baseurl = '/?page='
     return render(request, 'mainpage.html', {'questions': page.object_list, 
         'paginator': paginator,
         'page': page})
+
+def popular(request, *args, **kwargs):
+    question_set = models.Question.objects.order_by('-raiting', '-added_at')
+
+    paginator, page = paginate(request, question_set)
+    paginator.baseurl = '/popular/?page='
+    return render(request, 'mainpage.html', {'questions': page.object_list, 
+        'paginator': paginator,
+        'page': page})
+    
+def question (request, *args, **kwargs):
+
+    try: q_id = int(kwargs['question_id'])
+    except: q_id = -1
+    question = get_object_or_404(models.Question, id = q_id)
+    try: 
+        answers = models.Answer.objects.filter(question_id = q_id).order_by('-added_at')
+    except:
+        answers = None
+    return render(request, 'question.html', {'question': question, 
+        'answers': answers, })
     
 
 
