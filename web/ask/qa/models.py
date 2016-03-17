aad953da0e93d0482fc4ce0568b22d8cf7b3fc9e
smartybit <karpukhin@gmail.com>
2016-03-17 12:54:53 +0300

    Model changes to suit test on stepic.org #3

--- web/ask/qa/models.py
+++ web/ask/qa/models.py
@@ -5,7 +5,7 @@ class Question(models.Model):
     title = models.CharField(max_length=255)
     text = models.TextField()
     added_at = models.DateTimeField(blank=False, auto_now_add=True)
-    rating = models.IntegerField()
+    rating = models.IntegerField(default = 0)
     author = models.ForeignKey(User, on_delete = models.CASCADE, related_name = "asked_questions",)
     likes= models.ManyToManyField(User, related_name = "liked _questions")
     def __unicode__(self):
