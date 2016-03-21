1a2964570d3ffed611e4cd871265ca7c26ff7522
smartybit <karpukhin@gmail.com>
2016-03-21 12:44:01 +0000

    user signup seemes to work

--- web/ask/qa/forms.py
+++ web/ask/qa/forms.py
@@ -47,31 +47,34 @@ class AnswerForm(forms.Form):
         return self.cleaned_data['question_id']
 
 class SignupForm(UserCreationForm):
-
+    
+    email = forms.EmailField(required=True)
     class Meta:
         model = User
         fields = ("username", "email")
 
-    def clean_username(self):
-        user = super(UserCreationForm, self).save(commit=False)
-        username = user.username
-        raise username
-        username = username.strip()
+#    def clean_username(self):
+#        theuser = super(UserCreationForm, self).save(commit=False)
+#        username = theuser.username
+#        raise username
+#        username = username.strip()
 #        if 
 #        if  User.objects.filter(username__iexact=username).first() != None:
 #            raise forms.ValidationError("This login is already in use")
-        return username
+#        return username
 
     def clean_email(self):
 #        email = super(UserCreationForm, self).clean_email()
-        user = super(UserCreationForm, self).save(commit=False)
-        email = user.email 
+        #user = super(UserCreationForm, self).save(commit=False)
+        ce = self.cleaned_data
+        email = self.cleaned_data["email"] 
         if  User.objects.filter(email__iexact=email).first() != None:
             raise forms.ValidationError("This email is already in use ;-)")
         return email
 
     def save(self, commit = True):
         user = super(UserCreationForm, self).save(commit=False)
+        user.email = self.cleaned_data["email"]
         if commit:
             user.save()
         return user
